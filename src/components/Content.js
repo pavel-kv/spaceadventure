e7790258bb7075cedf06d4dc495ac3ebbad1359e
Pavel <103311846+pavel-kv@users.noreply.github.com>
2022-12-26 00:34:57 +0300

    Add files via upload

---
+++ src/components/Content.js
@@ -0,0 +1,7 @@
+const Content = {
+	render: (className = "") => {
+		return `<main class="main-content ${className}" id="mainContent"></main>`;
+	}
+};
+
+export default Content;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = {
+	render: (className = "") => {
+		return `
+					<footer class="footer ${className}">
+      			<p>&copy; 2022 Space Adventure Project</p>
+    			</footer>
+					`;
+	}
+};
+
+export default Footer;
